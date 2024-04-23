c487a4edc53b989f06d4b71187b9ed6b5d7fe0c0
Ammaarah Fakier <Ammaarah.Fakier@capaciti.org.za>
2024-04-23 16:07:24 +0200

    Join chat page style page

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ function App() {
 
   return (
     
-
+ <div className="outer_cont">
     <div className="App">
         <h3>Join a chat</h3>
        <div className="container">
@@ -40,6 +40,7 @@ function App() {
         />
         <button class="btn" onClick={joinRoom}>Join</button>
         <Chat socket={socket} username={username} room={room}/>
+    </div>
     </div>
     </div>
   );
